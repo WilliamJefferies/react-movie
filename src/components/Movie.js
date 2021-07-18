4c7c712e2168d3a9d7fc32a3fa3c1f19a96b3f22
William Jefferies <william@checkferret.com>
2021-07-18 10:49:53 +1000

    use movie fetch hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,24 @@
 import React from "react";
 
-const Movie = () => <div>Movie</div>;
+// config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+
+// components
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+
+// Hook
+import { useMoiveFetch } from "../hooks/useMovieFetch";
+
+// Image
+import NoImage from "../images/no_image.jpg";
+
+const Movie = () => {
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
+};
 
 export default Movie;
