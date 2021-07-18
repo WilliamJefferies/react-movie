86f334f9a4469b1b240d90bcabcfde03c6a23917
William Jefferies <william@checkferret.com>
2021-07-18 11:26:17 +1000

    breadcrumbs

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,6 +7,7 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 // components
 import Grid from "./Grid";
 import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
 
 // Hook
 import { useMoiveFetch } from "../hooks/useMovieFetch";
@@ -18,10 +19,11 @@ const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMoiveFetch(movieId);
 
-  console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong..</div>;
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
   );
 };
